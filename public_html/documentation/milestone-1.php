727671147cfe98dd119ceca984b99efecc307918
Robert Dominguez <dominguez.1st@gmail.com>
2019-01-23 13:41:11 -0700

    First Draft of writing

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,22 +8,40 @@
 		<div class="pag">
 			<h1>Purpose, Audience, and Goal</h1>
 			<ul>
-				<li><strong>Purpose: </strong></li>
-				<li><strong>Audience: </strong></li>
-				<li><strong>Goal: </strong></li>
+				<li><strong>Purpose: </strong>This project will be a professional portfolio website with which to demo my
+					full stack development skills</li>
+				<li><strong>Audience: </strong>This site is designed with a professional audience in mind, and is made to
+				target individuals within the web development community. Target audience to include: hiring managers,
+				recruiters, and peers</li>
+				<li><strong>Goal: </strong>The primary goal of this site is for users of the site initiate contact with me
+					in regards to employment. The secondary goal of this site is to showcase my work, and to contribute to a
+				polished professional online presence.</li>
 			</ul>
 		</div>
 		<div class="persona">
 			<h1>Persona</h1>
 			<ul>
-				<li><strong>Name: </strong></li>
-				<li><strong>Age: </strong></li>
-				<li><strong>Profession: </strong></li>
-				<li><strong>Technology: </strong></li>
-				<li><strong>Attitudes and Behaviors: </strong></li>
-				<li><strong>Frustrations and Needs: </strong></li>
-				<li><strong>Goals: </strong></li>
-				<li><strong>User Story: </strong></li>
+				<li><strong>Name: </strong>Steve Johnson</li>
+				<li><strong>Age: </strong>36</li>
+				<li><strong>Profession: </strong>Manager of ExcogitatorisWebsites.com, a company that is looking to hire a
+					full-time, in-house full stack developer.</li>
+				<li><strong>Technology: </strong>Windows user. Uses a custom built desktop with Windows 10, a HP Pavilion
+					laptop, and a Samsung S8 phone.</li>
+				<li><strong>Attitudes and Behaviors: </strong>Fast when skimming but if something catches his eye then he
+					will give a more detailed look at things. Steve uses LinkedIn and other such sites to help narrow down his
+					search.</li>
+				<li><strong>Frustrations and Needs: </strong>Steve wants to hire someone to help his website development
+					company grow and expand. Steve wants someone that can work full stack and also knows how to work with a
+					team. Portfolio websites are a good way to see just a person brings to the table but Steve is frustrated
+					that viewing such sites can take a while. Because of this, Steve has begun to skim the sites rather
+					quickly and will only look more in-depth if the site appears to show what he wants quickly, easily, and
+					in high quality.</li>
+				<li><strong>Goals: </strong>Steve's goal is to view various portfolios quickly and gauge whether a person is
+					a potential candidate based on what he sees. Steve will quickly view a portfolio and if at first glance
+					the site looks to be of a higher quality then Steve will look at the portfolio more closely. After viewing
+					detail Steve will decide if the candidate is worth contacting to continue the job application process.</li>
+				<li><strong>User Story: </strong>As a user, I would view a clean and easy to read portfolio website to see
+					if this person is a candidate for out full stack developer position.</li>
 			</ul>
 		</div>
 	</body>
