b272ad0aa31a5458511ecbcf64abf0781244218f
Robert Dominguez <dominguez.1st@gmail.com>
2019-02-06 08:38:49 -0700

    Added the desktop wireframe.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<div class="Content Strategy">
+			<h1>Content Strategy</h1>
+			<p>
+				My site will be multiple pages, it will have one main page and links to other pages that will host whatever
+				projects that I will want to host on the site. The main page will consist of the following sections:
+			</p>
+			<p>
+				<strong>Welcome: </strong>A brief one-line introduction that clearly states my name and role as a Fullstack
+				Developer.
+			</p>
+			<p>
+				<strong>About Me: </strong>This section will detail my professional history, core skill set, and professional
+				highlights. This section will showcase my strengths and personality. I will feature a contrasting sidebar
+				that lists my core skills and programming languages which will be easily readable at a glance. The tone of
+				voice will be personable and casual, as I am only interested in opportunities with individuals and organizations
+				that I am a good personality fit for.
+			</p>
+			<p>
+				<strong>Portfolio: </strong>I want this to be an animated, responsive image gallery containing 4-6 examples
+				of my best work. Gallery pop-up will include my role and contributions to each project, with iconography to
+				represent the technology used.
+			</p>
+			<p>
+				<strong>Contact: </strong>This section will contain a simple contact form, my professional email address,
+				and additional links to my professional social media profiles: LinkedIn, Twitter, GitHub, Dribbble. (These
+				will also be linked to in the footer site wide via icons.)
+			</p>
+		</div>
+		<div class="wireframe">
+			<h1>Wireframes</h1>
+			<p>
+				This site will feature one layout that will be fully responsive on small and large screens. These wireframes
+				were created using https://mockflow.com.
+			</p>
+			<img src="wireframe-desktop.png" alt="desktop wireframe">
+		</div>
+	</body>
+</html>
